7bd3b1d8d2e7a03307abad8cc699450b3a666d1b
Gurdeep <gurdeep.singh@auw.co.in>
2024-09-30 17:55:24 +0530

    adding front end

--- src/models/FcmToken.jsx
+++
@@ -1,9 +0,0 @@
-// models/FcmToken.js
-import mongoose from 'mongoose';
-
-const FcmTokenSchema = new mongoose.Schema({
-  userId: { type: String, required: true, unique: true }, // Add userId as a unique identifier
-  token: { type: String, required: true },
-});
-
-export default mongoose.models.FcmToken || mongoose.model('FcmToken', FcmTokenSchema);
