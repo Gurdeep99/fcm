6daf01ea4024a5a0afe96c35ef21443ab1b27f3e
Gurdeep <gurdeep.singh@auw.co.in>
2024-10-10 16:22:46 +0530

    add more categories

--- src/app/api/auw/category/route.jsx
+++ src/app/api/auw/category/route.jsx
@@ -26,176 +26,312 @@ export async function GET(request) {
         "title": "hamburger categories",
         "status": "enable",
         "items": [
-            {
-                
-                "$id": "6207a21d065fe270c516282e",
-                "image": "https://spidercmsstage.amarujala.com/assets/images/250x250/default_500.png",
-                "titles": "आपके लिए",
-                "key": "for_you",
-                "link": "/india-news",
-                "status": "enable",
-                "type": "same-page",
-                "text_color": "#000000",
-                "bg_color": "#000000",
-                "others": "",
-                "sub_menu_slug": "",
-                "label": "",
-                "title_hn": "",
-                "priority": 0
+          {
+            "_id": {
+                "$oid": "6207a21d065fe270c516282e"
             },
-            {
-                
-                "$id": "6207a21d065fe270c516282e1",
-                "image": "https://spidercmsstage.amarujala.com/assets/images/250x250/default_500.png",
-                "titles": "देश",
-                "key": "desh",
-                "link": "/india-news",
-                "status": "enable",
-                "type": "same-page",
-                "text_color": "#000000",
-                "bg_color": "#000000",
-                "others": "",
-                "sub_menu_slug": "",
-                "label": "",
-                "title_hn": "",
-                "priority": 0
+            "image": "https://spidercmsstage.amarujala.com/assets/images/250x250/default_500.png",
+            "titles": "आपके लिए",
+            "key": "For You",
+            "link": "/india-news",
+            "status": "enable",
+            "type": "same-page",
+            "text_color": "#000000",
+            "bg_color": "#000000",
+            "others": "",
+            "sub_menu_slug": "",
+            "label": "",
+            "title_hn": "",
+            "priority": 0
+        },
+        {
+            "_id": {
+                "$oid": "6207a21d065fe270c516282e1"
             },
-            {
-                
-                "$id": "6207a21d065fe270c516283d",
-                "image": "https://spidercmsstage.amarujala.com/assets/images/250x250/default_500.png",
-                "titles": "दुनिया",
-                "key": "duniya",
-                "link": "/world",
-                "status": "enable",
-                "type": "same-page",
-                "text_color": "#000000",
-                "bg_color": "#000000",
-                "others": "",
-                "sub_menu_slug": "",
-                "label": "",
-                "title_hn": "",
-                "priority": 1
+            "image": "https://spidercmsstage.amarujala.com/assets/images/250x250/default_500.png",
+            "titles": "देश",
+            "key": "desh",
+            "link": "/india-news",
+            "status": "enable",
+            "type": "same-page",
+            "text_color": "#000000",
+            "bg_color": "#000000",
+            "others": "",
+            "sub_menu_slug": "",
+            "label": "",
+            "title_hn": "",
+            "priority": 0
+        },
+        {
+            "_id": {
+                "$oid": "6207a21d065fe270c516283d"
             },
-            {
-                
-                "$id": "6207a21d065fe270c516282f",
-                "image": "https://spidercmsstage.amarujala.com/assets/images/250x250/default_500.png",
-                "titles": "मनोरंजन",
-                "key": "entertainment",
-                "link": "/entertainment",
-                "status": "enable",
-                "type": "same-page",
-                "text_color": "#000000",
-                "bg_color": "#000000",
-                "others": "",
-                "sub_menu_slug": "",
-                "label": "",
-                "title_hn": "",
-                "priority": 2
+            "image": "https://spidercmsstage.amarujala.com/assets/images/250x250/default_500.png",
+            "titles": "दुनिया",
+            "key": "duniya",
+            "link": "/world",
+            "status": "enable",
+            "type": "same-page",
+            "text_color": "#000000",
+            "bg_color": "#000000",
+            "others": "",
+            "sub_menu_slug": "",
+            "label": "",
+            "title_hn": "",
+            "priority": 1
+        },
+        {
+            "_id": {
+                "$oid": "6207a21d065fe270c516282f"
             },
-            {
-                
-                "$id": "6207a21d065fe270c5162830",
-                "image": "https://spidercmsstage.amarujala.com/assets/images/250x250/default_500.png",
-                "titles": "क्रिकेट",
-                "key": "cricket",
-                "link": "/cricket",
-                "status": "enable",
-                "type": "same-page",
-                "text_color": "#000000",
-                "bg_color": "#000000",
-                "others": "",
-                "sub_menu_slug": "",
-                "label": "",
-                "title_hn": "",
-                "priority": 3
+            "image": "https://spidercmsstage.amarujala.com/assets/images/250x250/default_500.png",
+            "titles": "मनोरंजन",
+            "key": "entertainment",
+            "link": "/entertainment",
+            "status": "enable",
+            "type": "same-page",
+            "text_color": "#000000",
+            "bg_color": "#000000",
+            "others": "",
+            "sub_menu_slug": "",
+            "label": "",
+            "title_hn": "",
+            "priority": 2
+        },
+        {
+            "_id": {
+                "$oid": "6207a21d065fe270c5162830"
             },
-            {
-                
-                "$id": "6207a21d065fe270c5162833",
-                "image": "https://spidercmsstage.amarujala.com/assets/images/250x250/default_500.png",
-                "titles": "कारोबार",
-                "key": "business",
-                "link": "/business",
-                "status": "enable",
-                "type": "same-page",
-                "text_color": "#000000",
-                "bg_color": "#000000",
-                "others": "",
-                "sub_menu_slug": "",
-                "label": "",
-                "title_hn": "",
-                "priority": 4
+            "image": "https://spidercmsstage.amarujala.com/assets/images/250x250/default_500.png",
+            "titles": "क्रिकेट",
+            "key": "cricket",
+            "link": "/cricket",
+            "status": "enable",
+            "type": "same-page",
+            "text_color": "#000000",
+            "bg_color": "#000000",
+            "others": "",
+            "sub_menu_slug": "",
+            "label": "",
+            "title_hn": "",
+            "priority": 3
+        },
+        {
+            "_id": {
+                "$oid": "6207a21d065fe270c5162833"
             },
-            {
-                
-                "$id": "6207a21d065fe270c5162831",
-                "image": "https://spidercmsstage.amarujala.com/assets/images/250x250/default_500.png",
-                "titles": "नौकरी",
-                "key": "jobs",
-                "link": "/jobs",
-                "status": "enable",
-                "type": "same-page",
-                "text_color": "#000000",
-                "bg_color": "#000000",
-                "others": "",
-                "sub_menu_slug": "",
-                "label": "",
-                "title_hn": "",
-                "priority": 5
+            "image": "https://spidercmsstage.amarujala.com/assets/images/250x250/default_500.png",
+            "titles": "कारोबार",
+            "key": "business",
+            "link": "/business",
+            "status": "enable",
+            "type": "same-page",
+            "text_color": "#000000",
+            "bg_color": "#000000",
+            "others": "",
+            "sub_menu_slug": "",
+            "label": "",
+            "title_hn": "",
+            "priority": 4
+        },
+        {
+            "_id": {
+                "$oid": "6207a21d065fe270c5162831"
             },
-            {
-                
-                "$id": "6207a21d065fe270c5162832",
-                "image": "https://spidercmsstage.amarujala.com/assets/images/250x250/default_500.png",
-                "titles": "शिक्षा",
-                "key": "education",
-                "link": "/education",
-                "status": "enable",
-                "type": "same-page",
-                "text_color": "#000000",
-                "bg_color": "#000000",
-                "others": "",
-                "sub_menu_slug": "",
-                "label": "",
-                "title_hn": "",
-                "priority": 6
+            "image": "https://spidercmsstage.amarujala.com/assets/images/250x250/default_500.png",
+            "titles": "नौकरी",
+            "key": "jobs",
+            "link": "/jobs",
+            "status": "enable",
+            "type": "same-page",
+            "text_color": "#000000",
+            "bg_color": "#000000",
+            "others": "",
+            "sub_menu_slug": "",
+            "label": "",
+            "title_hn": "",
+            "priority": 5
+        },
+        {
+            "_id": {
+                "$oid": "6207a21d065fe270c5162832"
             },
-            {
-                
-                "$id": "6207a21d065fe270c5162834",
-                "image": "https://spidercmsstage.amarujala.com/assets/images/250x250/default_500.png",
-                "titles": "टेक्नोलॉजी",
-                "key": "technology",
-                "link": "/technology",
-                "status": "enable",
-                "type": "same-page",
-                "text_color": "#000000",
-                "bg_color": "#000000",
-                "others": "",
-                "sub_menu_slug": "",
-                "label": "",
-                "title_hn": "",
-                "priority": 7
+            "image": "https://spidercmsstage.amarujala.com/assets/images/250x250/default_500.png",
+            "titles": "शिक्षा",
+            "key": "education",
+            "link": "/education",
+            "status": "enable",
+            "type": "same-page",
+            "text_color": "#000000",
+            "bg_color": "#000000",
+            "others": "",
+            "sub_menu_slug": "",
+            "label": "",
+            "title_hn": "",
+            "priority": 6
+        },
+        {
+            "_id": {
+                "$oid": "6207a21d065fe270c5162834"
             },
-            {
-                
-                "$id": "6207a21d065fe270c5162835",
-                "image": "https://spidercmsstage.amarujala.com/assets/images/250x250/default_500.png",
-                "titles": "ऑटो",
-                "key": "automobiles",
-                "link": "/automobiles",
-                "status": "enable",
-                "type": "same-page",
-                "text_color": "#000000",
-                "bg_color": "#000000",
-                "others": "",
-                "sub_menu_slug": "",
-                "label": "",
-                "title_hn": "",
-                "priority": 8
+            "image": "https://spidercmsstage.amarujala.com/assets/images/250x250/default_500.png",
+            "titles": "टेक्नोलॉजी",
+            "key": "technology",
+            "link": "/technology",
+            "status": "enable",
+            "type": "same-page",
+            "text_color": "#000000",
+            "bg_color": "#000000",
+            "others": "",
+            "sub_menu_slug": "",
+            "label": "",
+            "title_hn": "",
+            "priority": 7
+        },
+        {
+            "_id": {
+                "$oid": "6207a21d065fe270c5162835"
             },
+            "image": "https://spidercmsstage.amarujala.com/assets/images/250x250/default_500.png",
+            "titles": "ऑटो",
+            "key": "automobiles",
+            "link": "/automobiles",
+            "status": "enable",
+            "type": "same-page",
+            "text_color": "#000000",
+            "bg_color": "#000000",
+            "others": "",
+            "sub_menu_slug": "",
+            "label": "",
+            "title_hn": "",
+            "priority": 8
+        },
+        {
+            "_id": {
+                "$oid": "6207a21d065fe270c5162836"
+            },
+            "image": "https://spidercmsstage.amarujala.com/assets/images/250x250/default_500.png",
+            "titles": "ज्योतिष",
+            "key": "astrology",
+            "link": "/astrology",
+            "status": "enable",
+            "type": "same-page",
+            "text_color": "#000000",
+            "bg_color": "#000000",
+            "others": "",
+            "sub_menu_slug": "",
+            "label": "",
+            "title_hn": "",
+            "priority": 9
+        },
+        {
+            "_id": {
+                "$oid": "6207a21d065fe270c5162837"
+            },
+            "image": "https://spidercmsstage.amarujala.com/assets/images/250x250/default_500.png",
+            "titles": "खेल",
+            "key": "sports",
+            "link": "/sports",
+            "status": "enable",
+            "type": "same-page",
+            "text_color": "#000000",
+            "bg_color": "#000000",
+            "others": "",
+            "sub_menu_slug": "",
+            "label": "",
+            "title_hn": "",
+            "priority": 10
+        },
+        {
+            "_id": {
+                "$oid": "6207a21d065fe270c5162838"
+            },
+            "image": "https://spidercmsstage.amarujala.com/assets/images/250x250/default_500.png",
+            "titles": "हेल्थ एंड फिटनेस",
+            "key": "lifestyle-fitness",
+            "link": "/lifestyle/fitness",
+            "status": "enable",
+            "type": "same-page",
+            "text_color": "#000000",
+            "bg_color": "#000000",
+            "others": "",
+            "sub_menu_slug": "",
+            "label": "",
+            "title_hn": "",
+            "priority": 11
+        },
+        {
+            "_id": {
+                "$oid": "6207a21d065fe270c5162839"
+            },
+            "image": "https://spidercmsstage.amarujala.com/assets/images/250x250/default_500.png",
+            "titles": "फैशन",
+            "key": "fashion",
+            "link": "/fashion",
+            "status": "enable",
+            "type": "same-page",
+            "text_color": "#000000",
+            "bg_color": "#000000",
+            "others": "",
+            "sub_menu_slug": "",
+            "label": "",
+            "title_hn": "",
+            "priority": 12
+        },
+        {
+            "_id": {
+                "$oid": "6207a21d065fe270c516283a"
+            },
+            "image": "https://spidercmsstage.amarujala.com/assets/images/250x250/default_500.png",
+            "titles": "शक्ति",
+            "key": "shakti",
+            "link": "/shakti",
+            "status": "enable",
+            "type": "same-page",
+            "text_color": "#000000",
+            "bg_color": "#000000",
+            "others": "",
+            "sub_menu_slug": "",
+            "label": "",
+            "title_hn": "",
+            "priority": 13
+        },
+        {
+            "_id": {
+                "$oid": "6207a21d065fe270c516283b"
+            },
+            "image": "https://spidercmsstage.amarujala.com/assets/images/250x250/default_500.png",
+            "titles": "आस्था",
+            "key": "spirituality",
+            "link": "/spirituality",
+            "status": "enable",
+            "type": "same-page",
+            "text_color": "#000000",
+            "bg_color": "#000000",
+            "others": "",
+            "sub_menu_slug": "",
+            "label": "",
+            "title_hn": "",
+            "priority": 14
+        },
+        {
+            "_id": {
+                "$oid": "6207a21d065fe270c516283c"
+            },
+            "image": "https://spidercmsstage.amarujala.com/assets/images/250x250/default_500.png",
+            "titles": "बॉलीवुड",
+            "key": "bollywood",
+            "link": "/entertainment/bollywood",
+            "status": "enable",
+            "type": "same-page",
+            "text_color": "#000000",
+            "bg_color": "#000000",
+            "others": "",
+            "sub_menu_slug": "",
+            "label": "",
+            "title_hn": "",
+            "priority": 15
+        }
         ],
         "image": null,
         "created_by": "61bc2e5844336c57c1031b89",
